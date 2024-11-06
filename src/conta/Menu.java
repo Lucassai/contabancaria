020bd2cd6b66610eac595cbc9a8dada9f516c080
Lucas Oliveira <lucasvmo42@gmail.com>
2024-11-06 15:05:30 -0300

    Adicionar package model e classe conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,11 +1,18 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
     public static void main(String[] args) {
+
+        Conta c1 = new Conta(1,123,1,"Adriana",10000.0f);
+        c1.visualizar();
+        c1.sacar(12000.0f);
+        c1.visualizar();
+        c1.depositar(5000.0f);
+        c1.visualizar();
 
         Scanner leia = new Scanner(System.in);
 
@@ -13,7 +20,7 @@ public class Menu {
 
         while(true) {
 
-            System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+            System.out.println(Cores.TEXT_RED + Cores.ANSI_BLACK_BACKGROUND
                     + "*****************************************************");
             System.out.println("                                                     ");
             System.out.println("                BANCO DO BRAZIL COM Z                ");
