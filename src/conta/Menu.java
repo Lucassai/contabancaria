6477139334a7003323f96fa71b7f815cbc1881f3
Lucas Oliveira <lucasvmo42@gmail.com>
2024-11-06 09:31:01 -0300

    Task 1

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -85,9 +85,9 @@ public class Menu {
 
     public static void sobre() {
         System.out.println("\n*********************************************************");
-        System.out.println("Projeto Desenvolvido por: ");
+        System.out.println("Projeto Desenvolvido por : Lucas Oliveira");
         System.out.println("Generation Brasil - generation@generation.org");
-        System.out.println("github.com/conteudoGeneration");
+        System.out.println("github.com/Lucassai");
         System.out.println("*********************************************************");
     }
 
